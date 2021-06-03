89902ce281d76ce65ab2ddfc4a8e88ef3963bdb6
Andrew Malsack <drewmalsack@gmail.com>
2021-06-03 12:28:36 -0500

    Worked through end of 13
    Code is written through the end of section 13 of the class

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -12,13 +12,18 @@ public class SwimJavaConfigDemoApp {
 				new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		//get bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		//call method to get daily fortune
 		System.out.println(theCoach.getDailyFortune());
+		
+		// call our new methods
+		System.out.println(theCoach.getEmail());
+		
+		System.out.println(theCoach.getTeam());
 		
 		// close the context
 		context.close();
